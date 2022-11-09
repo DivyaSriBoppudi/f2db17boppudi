4a43ab8b4b9311ba7f367ae6266b61603869c214
Divya_sri_Boppudi <s556453@nwmissouri.edu>
2022-11-09 16:50:26 -0600

    Updated the POST request in colletion controller

--- controllers/candy.js
+++ controllers/candy.js
@@ -18,8 +18,24 @@ exports.candy_detail = function(req, res) {
 }; 
  
 // Handle Candy create on POST. 
-exports.candy_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Candy create POST'); 
+exports.candy_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Candy(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"candy_name":"Charms", "candy_flavor": "Cherry", "candy_cost":12} 
+    document.candy_name = req.body.candy_name; 
+    document.candy_flavor = req.body.candy_flavor; 
+    document.candy_cost = req.body.candy_cost; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }
 }; 
  
 // Handle Candy delete form on DELETE. 
@@ -43,4 +59,4 @@ exports.candy_view_all_Page = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
-}; 
+};
