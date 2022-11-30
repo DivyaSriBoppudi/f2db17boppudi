210c91d5ae302b0960951bdb54ec98418feefc56
Divya_sri_Boppudi <s556453@nwmissouri.edu>
2022-11-30 01:36:28 -0600

    Added authentication to protect endpoints

--- routes/candies.js
+++ routes/candies.js
@@ -2,6 +2,16 @@ var express = require('express');
 const candy_controlers= require('../controllers/candy'); 
 var router = express.Router(); 
  
+// A little function to check if we have an authorized user and continue on or  redirect to login. 
+
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET candies */ 
 router.get('/', candy_controlers.candy_view_all_Page ); 
 
@@ -12,7 +22,7 @@ router.get('/detail', candy_controlers.candy_view_one_Page);
 router.get('/create', candy_controlers.candy_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', candy_controlers.candy_update_Page);
+router.get('/update',secured, candy_controlers.candy_update_Page);
 
 /* GET delete candy page */ 
 router.get('/delete', candy_controlers.candy_delete_Page); 
